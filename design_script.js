5ea57c4c6d901cd5bcb6f8ebbabd8a6b1881691b
Raj Sathvara <109301347+Raj5222@users.noreply.github.com>
2024-11-17 21:13:31 +0530

    Update design_script.js

--- design_script.js
+++ design_script.js
@@ -1,91 +1,31 @@
-const form = document.getElementById('upload-form');
-const resultContainer = document.getElementById('result');
-const progress = document.getElementById('progress');
-const resultBtn = document.getElementById('result-btn');
-const resultShowBtn = document.getElementById('result-show');
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
 const progressSteps = document.querySelectorAll(".progress-step");
 
 let formStepsNum = 0;
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const formData = new FormData();
-  const image = document.getElementById('image').files[0];
-
-  if (image instanceof Blob) {
-    formData.append('image', image, 'uploaded_image.jpeg');
-    try {
-      const response = await fetch('https://image-detection-olo3.onrender.com', {
-        method: 'POST',
-        body: formData
-      });
-
-      const data = await response.json();
-      document.getElementById("data-title").textContent = "Detection Results";
-      const labelValues = document.getElementById("data");
-      data.faces.concat(data.objects).forEach(function (item) {
-        if (item.label) {
-          const listItem = document.createElement("li");
-          listItem.textContent = item.label;
-          labelValues.appendChild(listItem);
-        }
-      });
-
-      if (response.ok) {
-        const img = new Image();
-        img.onload = function () {
-          displayResult(data, img);
-        };
-        img.src = URL.createObjectURL(image);
-      } else {
-        displayError(data.error);
-      }
-    } catch (error) {
-      displayError('An error occurred while processing the request.');
-    }
-  } else {
-    displayError('Error uploading the image.');
-  }
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressbar();
+  });
 });
 
-function displayResult(data, image) {
-  resultContainer.innerHTML = '';
-  const canvas = document.createElement('canvas');
-  canvas.width = image.naturalWidth;
-  canvas.height = image.naturalHeight;
-  const ctx = canvas.getContext('2d');
-
-  ctx.drawImage(image, 0, 0);
-  ctx.strokeStyle = '#FF0000';
-  data.faces.forEach(face => {
-    ctx.strokeRect(face.x, face.y, face.width, face.height);
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum--;
+    updateFormSteps();
+    updateProgressbar();
   });
-
-  data.objects.forEach(obj => {
-    ctx.strokeRect(obj.x, obj.y, obj.width, obj.height);
-  });
-
-  resultContainer.appendChild(canvas);
-}
-
-function displayError(message) {
-  resultContainer.textContent = message;
-}
-
-resultBtn.addEventListener('click', () => {
-  document.getElementById('result-btn').style.display = 'block';
-});
-
-resultShowBtn.addEventListener('click', () => {
-  formStepsNum--;
-  updateFormSteps();
-  updateProgressbar();
 });
 
 function updateFormSteps() {
   formSteps.forEach((formStep) => {
-    formStep.classList.remove("form-step-active");
+    formStep.classList.contains("form-step-active") &&
+      formStep.classList.remove("form-step-active");
   });
 
   formSteps[formStepsNum].classList.add("form-step-active");
@@ -93,7 +33,7 @@ function updateFormSteps() {
 
 function updateProgressbar() {
   progressSteps.forEach((progressStep, idx) => {
-    if (idx <= formStepsNum) {
+    if (idx < formStepsNum + 1) {
       progressStep.classList.add("progress-step-active");
     } else {
       progressStep.classList.remove("progress-step-active");
@@ -101,5 +41,18 @@ function updateProgressbar() {
   });
 
   const progressActive = document.querySelectorAll(".progress-step-active");
-  progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+
+  progress.style.width =
+    ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
+
+// Result Button Hide/Show
+
+const btn = document.getElementById('result-show');
+
+btn.addEventListener('click', () => {
+  const box = document.getElementById('result-btn');
+  box.style.display = 'block';
+});
+
+//btn
